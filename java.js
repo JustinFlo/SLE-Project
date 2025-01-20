72c69dfda46c153b499d0d34aa33d8b9916a9933
JustinFlo <225jflores@frhsd.com>
2025-01-20 15:59:57 -0500

    Added in Swatch Cards

---
+++ java.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $(".card").on("mouseover", function () {
+        $(this).nextAll().addClass("prev");
+    });
+    $(".card").on("mouseout", function () {
+        $(this).nextAll().removeClass("prev");
+    });
+});
