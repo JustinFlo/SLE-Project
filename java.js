189bc47fc57886419d317e73b296614b8ab10c76
JustinFlo <225jflores@frhsd.com>
2025-02-07 09:20:53 -0500

    Added countdown

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('active');
+  });
+})
 
 $(document).ready(function () {
     $(".card").on("mouseover", function () {
@@ -7,3 +14,30 @@ $(document).ready(function () {
         $(this).nextAll().removeClass("prev");
     });
 });
+
+var targetDate = new Date('May 4, 2025 09:00:00').getTime();
+
+function getTime(){
+  var current = new Date().getTime();
+
+  difference = targetDate - current;
+
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+
+  var d = Math.floor(difference / (day));
+  var h = Math.floor((difference % (day)) / (hour));
+  var m = Math.floor((difference % (hour)) / (minute));
+  var s = Math.floor((difference % (minute)) / (second));
+
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+  getTime();
+})
